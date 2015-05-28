f6acf3095b847993a737cd8bf220369ba39e36bc
debbie balbo & audrie talbot <student@epicodus.com>
2015-05-28 14:01:20 -0700

    Pass first 3 specs.

--- lib/wordcount.rb
+++ lib/wordcount.rb
@@ -1,14 +1,25 @@
 class String
   define_method(:word_count) do |entered_word|
-      frequency = 0
-      sentence = self.split()
-      if sentence.include?(entered_word)
-        sentence.each() do |word|
-        if word.eql?(entered_word)
-          frequency = frequency + 1
+
+    entered_word = entered_word.downcase
+    entered_word = entered_word.gsub(/[!?@#$%^&*]/,"")
+    binding.pry
+    sentence = self.gsub(/[!?@#$%^&*]/,"")
+    frequency = 0
+
+    sentence = sentence.downcase.split()
+
+    if sentence.include?(entered_word)
+      sentence.each() do |word|
+
+      if word.eql?(entered_word)
+        frequency = frequency + 1
         end
+
       end
+
     frequency
+
     end
   end
 end
--- spec/wordcount_spec.rb
+++ spec/wordcount_spec.rb
@@ -8,4 +8,12 @@ describe('String#word_count') do
    expect(('I need coffee').word_count('coffee')).to(eq(1))
   end
 
+
+  it('ignores erroneous punctuation in multi-word string') do
+    expect(('Aquarium? balls!!').word_count('aquarium')).to(eq(1))
  end
+
+ it('it ignores erroneous punctuation the single word') do
+   expect(('chicken head').word_count('head$')).to(eq(1))
+ end
+end
